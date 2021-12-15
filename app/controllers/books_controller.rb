92424e06c684254ec50eba5cfd9cf44372272765
kaichi0618 <kaichi.1998.0618@gmail.com>
2021-12-15 14:46:12 +0000

    [add]新規投稿

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
   def index
@@ -9,5 +10,18 @@ class BooksController < ApplicationController
   end
 
   def show
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to new_book_path
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
