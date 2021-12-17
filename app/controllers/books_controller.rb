f09845a29969800e91bfcbba9a5e357fb51aebc8
kaichi0618 <kaichi.1998.0618@gmail.com>
2021-12-17 10:53:55 +0000

    [add]編集機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,20 @@ class BooksController < ApplicationController
   end
 
   def index
+    @books = Book.all
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      render edit
+    end
   end
 
   def show
