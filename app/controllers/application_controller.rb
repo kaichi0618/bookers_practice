12f70ead078c8fb3ef61e73d6715ec3cf7956212
kaichi0618 <kaichi.1998.0618@gmail.com>
2022-01-05 04:23:13 +0000

    [add]after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,10 @@ def configure_permitted_parameters
   devise_parameter_sanitizer.permit(:sign_up,keys:[:email]) # 注目
 end
 
+def after_sign_in_path_for(resource)
+  user_path(resource)
+end
+
 def after_sign_out_path_for(resource)
     root_path
 end
