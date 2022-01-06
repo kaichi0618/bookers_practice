c9c214bffe935ce13de49e3d95137148ed97bc9b
kaichi0618 <kaichi.1998.0618@gmail.com>
2022-01-06 08:53:21 +0000

    [finish]bookers

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'e51af647aa96380382fa40754c62fb0321501ac6e793d234d5b7827cf0ce6d1684faeaa20792285dd6f686557811aee36e41aa5b7a90d9551a2048a57e7d87be'
